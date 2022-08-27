8442e2df812dae4da2965d9c8ac7aa2295085522
michaelcoitim <michaelcoitim1@gmail.com>
2022-08-27 18:25:40 -0300

    exemplos de Variáveis de referencias

--- aula.js
+++ aula.js
@@ -1,5 +1,14 @@
-// console.log("Michael");
- let idade =30;
- let altura =176;
-let peso=89;
-console.log(idade , altura ,peso );
+// // console.log("Michael");
+//  let idade =30;
+//  let altura =176;
+// let peso=89;
+// console.log(idade , altura ,peso );
+// const sexo = "masculino";
+// console.log(sexo)
+
+//exemplos de Variáveis de referencias 
+let nome = "teste"; //string literal
+let idade = 25; // numeber literal 
+let estaAprovado = true; //boolean 
+let sobrenome ; // underfined não definido 
+let corSelecionada = null; // utilizado para zerar a variavel 
